034ae097be30729e7279284389645746fa0eecb8
SuperPaintman <SuperPaintmanDeveloper@gmail.com>
2016-08-29 04:24:02 +0500

    Removed extra comment

--- umka.lua
+++ umka.lua
@@ -12,7 +12,6 @@ local u = {}
 -- @param {Number|Boolean} [floating=0]   Specify returning a floating-point number.
 --
 -- @return {Number}  Returns the random number.
---function u.random(lower, upper, floating)
 function u.random(...)
   local args_count = select("#", ...)
   local lower     = select(1, ...)
